1de376682e32ebea801649d8bfe9a388e56690ea
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-05-17 12:43:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ function countdown(seconds, onFinish) {
     const seconds = remainingTime % 60;
 
     const timeLeft = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    process.stdout.write(`⏳ 等待 ${timeLeft} 再次运行...`);
+    process.stdout.write(`⏳ 等待 ${timeLeft} 再次运行... `);
 
     remainingTime--;
 
